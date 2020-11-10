cfcc5deb1a35b92ae6262ab09ddbcd583d77deaf
arifulkabir179 <72689991+arifulkabir179@users.noreply.github.com>
2020-11-10 19:32:26 +0600

    Update addbook.php

---
+++ MidLabTask/addbook.php
@@ -0,0 +1,81 @@
+<html>
+<head>
+	<title>Books</title>
+</head>
+<body>
+    <form action="" method="post" style="width: 600px;">
+	    <fieldset style="background-color: white;">
+	    	<table align="center">
+	    		<h1 align="center"> Add Books <h1>
+	    		<tr style="line-height: 60px;">
+	    			<td align="right">
+	    				Book Name:
+	    			</td>
+	    			<td>
+	    				<input type="text" name="bname">
+	    				
+	    			</td>
+	    		<tr style="line-height: 60px;">
+	    			<td align="right">
+	    				Category:
+	    			</td>
+	    			<td>
+	    			<select>
+					<option> Architecture </option>
+					<option> Sports </option>
+					<option> Astrology </option>
+					<option> Business </option>
+					</select>
+	    			</td>
+	    		</tr>
+	    		<tr style="line-height: 60px;">
+	    			<td align="right">
+	    				<p>Description:</p>
+	    			</td>
+	    			<td>
+	    				<input type="text" name="text">
+	    				
+	    			</td>
+	    		</tr>
+	    		<tr style="line-height: 60px;">
+	    			<td align="right">
+	    				Publisher:
+	    			</td>
+	    			<td>
+	    				<input type="text" name="pname">
+	    			
+	    			</td>
+	    		</tr>
+	    		<tr style="line-height: 60px;">
+	    			<td align="right">
+	    				ISBN:
+	    			</td>
+                    <td>
+                    	<input type="text" name="ename">
+                    	
+                    </td>
+	    		</tr>
+	    		<tr style="line-height: 60px;">
+	    			<td align="right">
+	    				Pages:
+	    			</td>
+	    			<td>
+	    				<input type="number" name="number">
+	    				
+	    			</td>
+	    		</tr>
+	    		<tr style="line-height: 60px;">
+	    			<td align="center">
+	    			    Price:
+	    			</td>
+	    			<td>
+	    				<input type="number" name="price">
+	    			</td>
+	    		</tr>
+	    		
+	    	
+	    	</table>
+	    </fieldset>
+    </form>
+</body>
+</html>
